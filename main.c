bb772be62de9cfac69ee779073d59e8e3e896d50
SargamSharma21 <sargamsharma012@gmail.com>
2026-07-01 10:15:02 +0000

    Add C program main.c

---
+++ main.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+void bubbleSort(int arr[],int n){
+  for(int i = 0; i < n-1; i++){
+   for(int j = 0; j < n-i-1; j++){
+     if(arr[j] > arr[j+1]){
+        int temp = arr[j];
+        arr[j]  =   arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
+}
+
+void generate_number(int arr[],int n){
+   for(int i = 0;i<n;i++){
+    arr[i] = rand() % 100000;
+   }
+}
+
+int main(){
+  srand(time(NULL));
+  int size1 = 40000;
+  int size2 = 80000;
+  int *arr1 = (int *)malloc(size1 * sizeof(int));
+  int *arr2 = (int *)malloc(size2 * sizeof(int));
+  clock_t start1 = clock();
+  bubbleSort(arr1,size1);
+  clock_t end1 = clock();
+  double time1 = (double)(end1-start1);
+  clock_t start2 = clock();
+  bubbleSort(arr2,size2);
+  clock_t end2 = clock();
+  double time2 = (double)(end2 - start2);
+  printf("\n%f\n",time2/time1);
+  return 0;
+}
+
