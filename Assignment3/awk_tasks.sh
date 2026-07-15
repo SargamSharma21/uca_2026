d0d788d0c4bcf7cfa7074863299a5673fdcb2326
SargamSharma21 <sargamsharma012@gmail.com>
2026-07-15 15:36:26 +0000

    Changes in shell Scripting

--- Assignment3/awk_tasks.sh
+++ Assignment3/awk_tasks.sh
@@ -7,39 +7,39 @@ Eva,Engineering,55,35
 EOF
 
 
-# echo "Task1"
-# awk -F',' '{print $1,$2}' employees.txt
+echo "Task1"
+awk -F',' '{print $1,$2}' employees.txt
 
-# echo 
+echo 
 
-# echo 'Task2'
-# awk -F',' '$2=="Engineering"' employees.txt
+echo 'Task2'
+awk -F',' '$2=="Engineering"' employees.txt
 
-# echo
+echo
 
-# echo 'Task3'
-# awk -F',' '{printf "%s gross pay: $%d\n" , $1 , $3*$4}' employees.txt
+echo 'Task3'
+awk -F',' '{printf "%s gross pay: $%d\n" , $1 , $3*$4}' employees.txt
 
-# echo
+echo
 
-# echo 'Task3'
-# awk -F',' '{printf "%s gross pay: $%d\n" , $1 , $3*$4}' employees.txt
+echo 'Task3'
+awk -F',' '{printf "%s gross pay: $%d\n" , $1 , $3*$4}' employees.txt
 
-# echo
+echo
 
-# echo 'Task4'
-# awk -F',' '{printf "%d (%d fields): %s\n" , NR , NF,$0}' employees.txt
+echo 'Task4'
+awk -F',' '{printf "%d (%d fields): %s\n" , NR , NF,$0}' employees.txt
 
-# echo
+echo
 
-# echo 'Task5'
-# awk -F',' '
-# BEGIN{
-#    total = 0
-# }
-# $2=="Engineering"{
-#    total+= $3*$4
-# }
-# END{
-#    printf("Total Engineering Payroll: $%d\n",total)
-# }' employees.txt
+echo 'Task5'
+awk -F',' '
+BEGIN{
+   total = 0
+}
+$2=="Engineering"{
+   total+= $3*$4
+}
+END{
+   printf("Total Engineering Payroll: $%d\n",total)
+}' employees.txt
