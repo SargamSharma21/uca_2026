e861aa6f8626c2117b490471d362b95949adfeef
SargamSharma21 <sargamsharma012@gmail.com>
2026-08-05 17:04:31 +0000

    Added new Assignment

---
+++ Assignment6/setMatrixZero.java
@@ -0,0 +1,57 @@
+public class setMatrixZero {
+    public static void main(String[] args) {
+        int[][] matrix = {{0,1,2,0},
+                          {3,4,5,2},
+                          {1,3,1,5}};
+        boolean firstRow = false;
+        boolean firstCol = false;
+        for(int i = 0 ; i < matrix.length ; i++) {
+            if(matrix[i][0] == 0) {
+                firstCol = true;
+            }    
+        }
+        for(int i = 0 ; i < matrix[0].length ; i++) {
+            if(matrix[0][i] == 0) {
+                firstRow = true;
+            }    
+        }
+        for(int i = 0 ; i < matrix.length ; i++) {
+            for(int j = 0 ; j < matrix[i].length ; j++) {
+                if(matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        for(int i = 1 ; i < matrix.length ; i++) {
+            if(matrix[i][0] == 0) {
+                for(int j = 1 ; j < matrix[i].length ; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        for(int i = 1 ; i < matrix[0].length ; i++) {
+            if(matrix[0][i] == 0) {
+                for(int j = 1 ; j < matrix.length ; j++) {
+                    matrix[j][i] = 0;
+                }
+            }
+        }
+        if(firstRow) {
+            for(int i = 0; i < matrix[0].length;i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        if(firstCol) {
+            for(int i = 0; i < matrix.length;i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        for(int i = 0 ; i < matrix.length ; i++) {
+            for(int j = 0 ; j < matrix[i].length ; j++) {
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
